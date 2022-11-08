9cff87b634c6c0731339c5bb24e7c6a9ed6c1c5f
hungpvph25929 <hungpvph25929@fpt.edu.vn>
2022-11-08 22:33:46 +0700

    file moi

---
+++ src/main/java/Utilities/HibernatUtil.java
@@ -0,0 +1,41 @@
+package Utilities;
+
+import java.util.Properties;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
+public class HibernatUtil {
+
+    private static final SessionFactory FACTORY;
+
+    static {
+        Configuration conf = new Configuration();
+
+        Properties properties = new Properties();
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.SQLServerDialect");
+        properties.put(Environment.DRIVER, "com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        properties.put(Environment.URL, "jdbc:sqlserver://localhost:1433;databaseName=FINALASS_FPOLYSHOP_FA22_SOF205__SOF2041");
+        properties.put(Environment.USER, "sa");
+        properties.put(Environment.PASS, "25082003");
+        properties.put(Environment.SHOW_SQL, "true");
+
+        conf.setProperties(properties);
+
+        ServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .applySettings(conf.getProperties()).build();
+        FACTORY = conf.buildSessionFactory(registry);
+
+    }
+
+    public static SessionFactory getFACTORY() {
+        return FACTORY;
+    }
+
+    public static void main(String[] args) {
+        getFACTORY();
+    }
+
+}
