5ca4e7da3ccd6b7160adb3f815c4ab17443fe8fb
hungpvph25929 <hungpvph25929@fpt.edu.vn>
2022-11-19 14:39:03 +0700

    no message

--- src/main/java/domainModel/ChiTietSp.java
+++ src/main/java/domainModel/ChiTietSp.java
@@ -60,8 +60,8 @@ public class ChiTietSp implements Serializable {
     private BigDecimal gia;
 
     @Column(name = "CreatedDate")
-    private Date CreatedDate;
+    private Date createdDate;
 
     @Column(name = "LastModifiedDate")
-    private Date LastModifiedDate;
+    private Date lastModifiedDate;
 }
