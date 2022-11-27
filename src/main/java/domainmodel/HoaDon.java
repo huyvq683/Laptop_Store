9aa018859b50b34787ebefa34de6df184d6f4058
PHANQUOCHUY2003\huyxo <huypqph21970@fpt.edu.vn>
2022-11-27 22:16:13 +0700

    ViewSanPham

---
+++ src/main/java/domainmodel/HoaDon.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domainmodel;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author FPT
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "HoaDon")
+public class HoaDon implements Serializable {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "Id")
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "IdKH")
+    private KhachHang idKH;
+
+    @ManyToOne
+    @JoinColumn(name = "IdNV")
+    private NhanVien idNV;
+
+    @Column(name = "Ma")
+    private String ma;
+
+    @Column(name = "NgayTao")
+    private Date ngayTao;
+
+    @Column(name = "TienKhachTra")
+    private BigDecimal tienKhacTra;
+
+    @Column(name = "TienCK")
+    private BigDecimal tienCK;
+
+    @Column(name = "TienThua")
+    private BigDecimal tienThua;
+
+    @Column(name = "TongTien")
+    private BigDecimal tongTien;
+
+    @Column(name = "HinhThuc")
+    private BigDecimal hinhThuc;
+
+    @Column(name = "TinhTrang")
+    private int tinhTrang;
+
+    @Column(name = "CreatedDate")
+    private Date createdDate;
+
+    @Column(name = "LastModifiedDate")
+    private Date lastModifiedDate;
+
+}
---
+++ src/main/java/domainmodel/SanPham.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domainmodel;
+
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ *
+ * @author FPT
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "SanPham")
+@ToString
+public class SanPham implements Serializable {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "Id")
+    private UUID id;
+
+    @Column(name = "Ma")
+    private String ma;
+
+    @Column(name = "Ten")
+    private String ten;
+
+    @Column(name = "CreatedDate")
+    private Date createdDate;
+
+    @Column(name = "LastModifiedDate")
+    private Date alstModifiedDate;
+
+    @Column(name = "TinhTrang")
+    private int tinhTrang;
+
+    public SanPham(String ma, String ten, Date createdDate, Date alstModifiedDate, int tinhTrang) {
+        this.ma = ma;
+        this.ten = ten;
+        this.createdDate = createdDate;
+        this.alstModifiedDate = alstModifiedDate;
+        this.tinhTrang = tinhTrang;
+    }
+
+    public String conVert(Date x) {
+        DateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+        return format.format(x);
+    }
+
+    public SanPham(String ma, String ten) {
+        this.ma = ma;
+        this.ten = ten;
+    }
+
+}
