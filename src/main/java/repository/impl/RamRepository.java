ffbb5b4fdd7c15eff0da39b828d1e8363038b9eb
PHANQUOCHUY2003\huyxo <huypqph21970@fpt.edu.vn>
2022-12-08 02:52:14 +0700

    PanelSanPham

--- src/main/java/repository/impl/RamRepository.java
+++ src/main/java/repository/impl/RamRepository.java
@@ -22,7 +22,7 @@ public class RamRepository {
     public List<Ram> getAll() {
         List<Ram> lists = new ArrayList<>();
         try (Session session = HibernateUtil.getFACTORY().openSession()) {
-            Query query = session.createQuery("From Ram");
+            Query query = session.createQuery("From Ram ORDER BY Ma DESC");
             lists = query.getResultList();
         } catch (Exception e) {
             e.printStackTrace(System.out);
