c21510999bea59c3d2187d8161cfae74fb6a3a28
hieundph25894 <hieundph25894@fpt.edu.vn>
2022-12-09 13:19:10 +0700

    hieundph25894 fix lỗi cbb năm bên biểu đồ thống kê

--- src/main/java/view/PanelThongKe.java
+++ src/main/java/view/PanelThongKe.java
@@ -1,7 +1,5 @@
 package view;
 
-
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
@@ -1487,8 +1485,14 @@ public class PanelThongKe extends javax.swing.JPanel {
             dcbmThangBieuDo = new DefaultComboBoxModel();
             lbChonBD.setText("Chọn năm :");
             int nam = Integer.parseInt(dateFor.format(new Date()).substring(6, 10));
-            for (int i = serviceThongKe.namBatDau(); i <= nam; i++) {
-                dcbmThangBieuDo.addElement(i);
+            if (serviceThongKe.namBatDauDoanhThu() != 0) {
+                for (int i = serviceThongKe.namBatDauDoanhThu(); i <= nam; i++) {
+                    dcbmThangBieuDo.addElement(i);
+                }
+            } else {
+                for (int i = 2020; i <= nam; i++) {
+                    dcbmThangBieuDo.addElement(i);
+                }
             }
             cbbBieuDoChonThang.setModel(dcbmThangBieuDo);
         }
